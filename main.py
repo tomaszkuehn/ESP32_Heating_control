429d8294ef6f6c7f531b23348237ba782ef1e3ed
Tomasz Kuehn <tomasz.kuehn@gmail.com>
2022-04-21 23:49:57 +0200

    Updating to ESP32

--- main.py
+++ main.py
@@ -1,20 +1,55 @@
-#!/usr/bin/env python
-
 import os
 import time
+import ntptime
 import sys
-import serial
+#import serial
 import binascii
-import threading
-from threading import Thread
-import Queue
+import socket
+import select
+#import threading
+#from threading import Thread
+#import Queue
 
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
+import machine
+import network
+
+import esp
+esp.osdebug(None)
+
+import gc
+gc.collect()
+
+ssid = 'NCC'
+password = 'password'
+
+station = network.WLAN(network.STA_IF)
+
+station.active(True)
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+  pass
+
+print('Connection successful')
+print(station.ifconfig())
+
+blue_led = machine.Pin(2, machine.Pin.OUT)
+blue_led.value(1)
+
+#opening socket for http
+mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+mysocket.bind(('', 80))
+mysocket.listen(5)
+mysocket.setblocking(0)
+#mysocket.settimeout(0.5)
+
+#os.system('modprobe w1-gpio')
+#os.system('modprobe w1-therm')
 
 #configuration parameters
-temp_sensor1 = '/sys/bus/w1/devices/28-0000031bd290/w1_slave'
-temp_sensor = '/sys/bus/w1/devices/28-0000031bc16f/w1_slave'
+temp_sensor1 = 1 #'/sys/bus/w1/devices/28-0000031bd290/w1_slave'
+temp_sensor = 0 #'/sys/bus/w1/devices/28-0000031bc16f/w1_slave'
 periodic_run_interval = 240 # in minutes
 booster_delay = 48 #in ticks, set 0 to disable
 
@@ -30,7 +65,36 @@ manual_pause = 0
 manual_stop  = 0 
 temp_shift = 0
 booster      = 0
+temp_avg = 0        #actual temp
 
+def web_page():
+  if 1 == 1:
+    gpio_state="ON"
+  else:
+    gpio_state="OFF"
+  global heating
+  gc.collect()
+  memf = gc.mem_free()
+  html = """<html><head> <title>ESP Web Server</title> <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="data:,"> <style>html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
+  h1{color: #0F3376; padding: 2vh;}p{font-size: 1.5rem;}.button{display: inline-block; background-color: #e7bd3b; border: none; 
+  border-radius: 4px; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
+  .button2{background-color: #4286f4;}</style></head><body> <h1>ESP Web Server</h1>
+  <p>TIME/MEM: """+str(round(time.time()))+""" / """ +str(memf)+ """</p>
+  <p>HEATING ON: <strong>HEATING"""+str(heating)+"""</strong></p>
+  <p>ACTUAL TEMPP: <strong>TEMP</strong></p>
+  <p><a href="/?led=on"><button class="button">ON</button></a></p>
+  <p><a href="/?led=off"><button class="button button2">OFF</button></a></p></body></html>"""
+  return html
+
+def time_sync():
+    try:
+        ntptime.settime()
+        print("Time adjusted: %s" %str(time.localtime()))
+    except:
+        print("NTP fail")
+        
+"""
 #configure serial port to send messages
 ser = serial.Serial (
     port='/dev/ttyAMA0',
@@ -40,7 +104,9 @@ ser = serial.Serial (
     bytesize=serial.EIGHTBITS,
     timeout=1
 )
+"""
 
+"""
 def temp_raw():
     try:
         f = open(temp_sensor, 'r')
@@ -51,10 +117,11 @@ def temp_raw():
     lines = f.readlines()
     f.close()
     return lines
-
+"""
 
 def read_temp(num, out_queue):
-
+    out_queue.append(1200)
+"""
     lines = temp_raw()
     timeout = 0
     while ((timeout < 10) and (lines[0].strip()[-3:] != 'YES')):
@@ -71,7 +138,7 @@ def read_temp(num, out_queue):
         #return temp_c
         print "Read temp TC:%f\n" % temp_c
         out_queue.put(temp_c)
-    
+"""    
 
     
 def heating_switch(heating):
@@ -96,9 +163,9 @@ def heating_switch(heating):
       packet.append(0xDD)
       packet.append(0xFA)
 
-   print binascii.hexlify(packet)
+   print(packet) #binascii.hexlify(packet)
 #comment the line below to avoid any messages sent
-   ser.write(packet)
+#   ser.write(packet)
 
 
 #heating_switch(0)
@@ -134,10 +201,10 @@ def process_msg(msg):
         temp_shift = -200
 
 
-temp_arr     = [];
+temp_arr     = []
 for i in range (0,720):
     temp_arr.append(0)
-heat_arr     = [];
+heat_arr     = []
 for i in range (0,720):
     heat_arr.append(0)
 
@@ -151,24 +218,27 @@ for i in range (0,24):
 
 
 def read_hour_arr():
-    f = open("/user/hour.txt","r")
-    f1 = f.readlines()
-    for x in f1:
-        if x[0] != '#':
-            utime,uon,uoff = x.split(",") 
-            hour_arr[int(utime)]=[int(uon), int(uoff)] 
+#    f = open("/user/hour.txt","r")
+#    f1 = f.readlines()
+#    for x in f1:
+#        if x[0] != '#':
+    for x in range(0,24):
+        uon,uoff = 1500,1600
+        hour_arr[x]=[int(uon), int(uoff)] 
     
-    print hour_arr[7][0]," ",hour_arr[7][1]+1
+    print(hour_arr[7][0]," ",hour_arr[7][1]+1)
     
     
 read_hour_arr()
-print hour_arr
-time.sleep(5)
-    
-my_queue = Queue.Queue()
+print(hour_arr)
+time.sleep(1)
+
+time_sync() 
+my_queue = [] #Queue.Queue()
 seconds = round(time.time())
 read_hour = 0
 
+"""
 #define system pipe to receive commands
 pipe_path = "/tmp/heating_pipe"
 if not os.path.exists(pipe_path):
@@ -176,41 +246,46 @@ if not os.path.exists(pipe_path):
 os.chmod(pipe_path, 0o777)    
 pipe_fd = os.open(pipe_path, os.O_RDONLY | os.O_NONBLOCK)
 pipe = os.fdopen(pipe_fd)
+"""
 
 # MAIN LOOP #
 
 while True:
     systime = time.localtime(time.time())
-    print systime.tm_hour,':',systime.tm_min, " ", tick, " While loop..."
-    print "Manual RUN:", manual_run, " Manual STOP:", manual_stop, " Periodic RUN:", periodic_run, " PAUSE:", manual_pause, " Booster:", booster
-    if ((systime.tm_min%5 == 0) and (read_hour == 0)):
+    #print (systime.tm_hour,':',systime.tm_min, " ", tick, " While loop...")
+    print ("Manual RUN:", manual_run, " Manual STOP:", manual_stop, " Periodic RUN:", periodic_run, " PAUSE:", manual_pause, " Booster:", booster)
+    if ((systime[4]%5 == 0) and (read_hour == 0)):
         read_hour_arr()
         read_hour = 1
-    if (systime.tm_min%5 != 0):
+    if (systime[4]%5 != 0):
         read_hour = 0
-    temp_on  = hour_arr[systime.tm_hour][0] + temp_shift
-    temp_off = hour_arr[systime.tm_hour][1] + temp_shift
-    print "Temp range ", temp_on,"-",temp_off
-
-#read 1-wire    
-    t = Thread(target=read_temp, args=(1, my_queue))
-    t.daemon = True
-    t.start()
-    t.join(2)
-    if t.is_alive():
-        print "Alive..."
-        t1 = Thread(target=read_temp, args=(1, my_queue))
-        t1.daemon = True
-        t1.start()
-        t1.join(2)
-        if t1.is_alive():
-            print "Alive2..."
-            print threading.active_count()
-#            continue
-#        continue
+    temp_on  = hour_arr[systime[3]][0] + temp_shift
+    temp_off = hour_arr[systime[3]][1] + temp_shift
+    print ("Temp range ", temp_on,"-",temp_off)
+    #read 1-wire    
+    """
+        t = Thread(target=read_temp, args=(1, my_queue))
+        t.daemon = True
+        t.start()
+        t.join(2)
+        if t.is_alive():
+            print "Alive..."
+            t1 = Thread(target=read_temp, args=(1, my_queue))
+            t1.daemon = True
+            t1.start()
+            t1.join(2)
+            if t1.is_alive():
+                print "Alive2..."
+                print threading.active_count()
+    #            continue
+    #        continue
+    """
+    read_temp(1, my_queue)
 #read until queue is empty
-    while not my_queue.empty():
-        tt = my_queue.get()
+    tt = my_queue.pop(0)
+    while tt:
+        
+
 #current temp in tt
         if (tt < -1000):
             tt = temp_avg_arr[0]
@@ -221,8 +296,8 @@ while True:
         temp_avg_arr.pop(0)
         temp_avg_arr.append(tt)
 
-        print temp_avg_arr
-
+        print(temp_avg_arr)
+        tt = 0
 #calculate average temperature over last minute
         temp_avg = 0
         for i in range (0, 12):
@@ -259,7 +334,7 @@ while True:
 #run heating periodically
         hh = 0
         if(heating == 0):
-            ppi = 720 - periodic_run_interval / 2;
+            ppi = round(720 - periodic_run_interval / 2)
             for i in range (ppi,720):
                 hh = hh + heat_arr[i]
             if ( hh == 0 ):
@@ -284,7 +359,7 @@ while True:
             heating = 0
         if(heating > 1):
             heating = 1
-        print "Heating switch:%d " % heating
+        print("Heating switch:%d " % heating)
         heating_switch(heating)
 
 #every two minutes    
@@ -305,6 +380,7 @@ while True:
                 manual_pause = manual_pause -1               
 
 #create www
+        """
         ff = open("/var/www/html/temp.json","w+")
         ff.write("{\n")
         ff.write("\"TEMP_ARR\":[0")
@@ -325,10 +401,36 @@ while True:
     
         print "\n"
         sys.stdout.flush()
-            
+        """            
         while True:
+            #handle http
+            ready = select.select([mysocket], [], [], 1)
+            if ready[0]:
+                #data = mysocket.recv(4096)
+                conn, addr = mysocket.accept()
+                print('Got a connection')
+                request = conn.recv(1024)
+                request = str(request)
+                print('Content = %s' % request)
+                """
+                led_on = request.find('/?led=on')
+                led_off = request.find('/?led=off')
+                if led_on == 6:
+                    print('LED ON')
+                    #led.value(1)
+                if led_off == 6:
+                    print('LED OFF')
+                    #led.value(0)
+                """
+                response = web_page()
+                conn.send('HTTP/1.1 200 OK\n')
+                conn.send('Content-Type: text/html\n')
+                conn.send('Connection: close\n\n')
+                conn.sendall(response)
+                conn.close()
             # check command over pipe
             # to send command use: echo "command" > /tmp/heating_pipe
+            """
             message = pipe.read()
             if message:
                 print("Received command(s): '%s'" % message)
@@ -336,12 +438,18 @@ while True:
                 for msg in msgs:
                     if msg:
                         process_msg(msg)
-            time.sleep(0.2)
+            """            
+            blue_led.value(1)            
+            time.sleep(0.01)
+            blue_led.value(0)
+            time.sleep(0.09)
             # replace the condition below for accelerated testing
             # if True:
             if (round(time.time()) - seconds >= 10):
-                break;
+                break
         seconds = round(time.time())
+        print (seconds)
+        print ("---")
         tick = tick + 1
 
 
